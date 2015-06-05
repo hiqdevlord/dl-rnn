4bc3652cfecb115816bd93f4269379ba233c76d2
hiqdevlord <hiqdevlord@gmail.com>
2015-06-05 19:12:39 +0200

    added lstm.py, renamed files and modified original tutorial (minimal_RNN)

--- scan_tutorial_solutions/sol_theano_reduce_monomials_from_map.py
+++
@@ -1,5 +0,0 @@
-test, updates = theano.reduce(add,
-                              sequences=monomial_expr,
-                              outputs_info=np.float64(0))
-
-f_poly = theano.function([x], test)
--- scan_tutorial_solutions/sol_theano_reduce_multi_output.py
+++
@@ -1,5 +0,0 @@
-both_expressions, updates = theano.reduce(return_args_and_accumulate_squares_and_ints,
-                                          sequences=T.arange(n), 
-                                          outputs_info=[
-            None, None, None, np.int32(0), np.cast['int32'](0)])
-f_both = theano.function([n], both_expressions)
